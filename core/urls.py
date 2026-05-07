346f5e6eb32322e5702560604f5fcc3fa6bdb7a9
Aylin Millape <aylin.millape@virginiogomez.cl>
2026-05-07 19:44:53 -0400

    Agregadas vistas y templates

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('juegos/', views.juegos, name='juegos'),
+]
