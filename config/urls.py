807b9aee574c41ce60ba7feb6061419e3bce5086
Aylin Millape <aylin.millape@virginiogomez.cl>
2026-05-07 20:47:59 -0400

    Arreglado error de sintaxis en urls

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 
 from django.urls import path
@@ -22,8 +23,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+]
 
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
