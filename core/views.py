43282ebb62c8845942ab3a4b98cda2574eec5fcb
mackarena <mackarena.contreras@virginiogomez.cl>
2026-05-08 12:57:47 -0400

    vista inteligente

--- core/views.py
+++ core/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from .models import Juego
 
 
 def home(request):
     return render(request, 'home.html')
 
 
-@login_required  # solo usuarios logueados pueden entrar
+def juegos(request):
+    # Si el usuario inició sesión, ve todos los juegos
+    if request.user.is_authenticated:
+        juegos = Juego.objects.all()
+    else:
+        # Si es público, solo ve los que marcaste como 'es_publico'
+        juegos = Juego.objects.filter(es_publico=True)
+
+    return render(request, 'juegos.html', {'juegos': juegos})
+
+
+@login_required  # Esto protege el área privada
 def perfil(request):
     return render(request, 'perfil.html')
-
-
-def juegos(request):
-    return render(request, 'juegos.html')
