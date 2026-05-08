d006375a27a706fa23b22fb50319c5699056d1e8
mackarena <mackarena.contreras@virginiogomez.cl>
2026-05-08 15:59:33 -0400

    registro y seguridad

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('perfil/', views.perfil, name='perfil'),
     path('juegos/', views.juegos, name='juegos'),
+    path('registro/', views.registro, name='registro'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Juego
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def home(request):
@@ -21,3 +23,17 @@ def juegos(request):
 @login_required  # Esto protege el área privada
 def perfil(request):
     return render(request, 'perfil.html')
+
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'¡Cuenta creada para {username}! Ya puedes iniciar sesión.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registro.html', {'form': form})
